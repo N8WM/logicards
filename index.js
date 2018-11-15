c0cf6c2c78235710821c31702679438173a6eb3a
Nathan McCutchen <n8wm@icloud.com>
2018-11-15 11:15:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+  /**
+  socket.emit('request', ####); // emit an event to the socket
+  io.emit('broadcast', ####); // emit an event to all connected sockets
+  socket.on('reply', function(){ #### }); // listen to the event
+  **/
+  console.log("connection initiated");
+});
+
 app.use(express.static('public'));
 
 http.listen(port, () => console.log('Example app listening on port ${port}!'));

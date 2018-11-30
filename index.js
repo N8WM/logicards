f8bbcb46eeb028b7d1a03fd4c2f2d292dcdc57a6
Nathan McCutchen <n8wm@icloud.com>
2018-11-29 20:26:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const /*port = 8080;*/ process.env.PORT;
+const port = /*8080;*/ process.env.PORT;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
